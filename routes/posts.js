62c1363bcb129fd1e8e10ba3a266416899eb5015
lejodev <alejandro.rv97@gmail.com>
2021-04-17 18:18:29 -0500

    Simpe CRUD  completed

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postSchema = require("../model/Post");
 
+// Get posts
 router.get("/", async (req, res) => {
   try {
     const posts = await postSchema.find();
@@ -14,6 +15,17 @@ router.get("/", async (req, res) => {
   res.send("EXCLELLENT MAN, LIKE AT ALL. YEAHHH");
 });
 
+// Get a specific post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await postSchema.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).send("Error: " + error);
+  }
+});
+
+// Create a new post
 router.post("/", (req, res) => {
   const post = new postSchema({
     title: req.body.title,
@@ -29,16 +41,29 @@ router.post("/", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
 
-  // post.save((err, data) => {
-  //   if (err) {
-  //     res.status(400);
-  //     console.log(err);
-  //   } else {
-  //     res.status(200).send("Post added successfully");
-  //     console.log("data added successfully");
-  //   }
-  // });
+// Update a post
+router.patch("/:id", async (req, res) => {
+  try {
+    const updatedPost = await postSchema.updateOne(
+      { _id: req.params.id },
+      { $set: { title: req.body.title } }
+    );
+    res.json(updatedPost).status(200);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedPost = await postSchema.remove({ _id: req.params.id });
+    res.status(200).json(deletedPost);
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
 });
 
 module.exports = router;
